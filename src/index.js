fdbc1682ee7ee37ab6d5c015073b7fac243964f0
Roman Pasternak <38011782+RomanPasternak@users.noreply.github.com>
2020-11-11 01:35:00 +0200

    v2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('ffff')
+
+const getResorse = async (url) => {
+    const res = await fetch(url);
+
+    if ( !res.ok ) {
+        throw new Error(`Could not fetch ${url}, received ${res.status}`);
+    };
+
+    return await res.json();
+};
+
+getResorse('https://swapi.dev/api/people/1/')
+    .then((body) => {
+        console.log(body);
+    }).catch((err) => {
+        console.error("Could  not fetch", err);
+    });
