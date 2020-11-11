56ca76a67a0a7c7f38221ff00d5e02da8685b67e
Roman Pasternak <38011782+RomanPasternak@users.noreply.github.com>
2020-11-11 02:04:57 +0200

    new function get API

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
 
-const getResorse = async (url) => {
-    const res = await fetch(url);
-
-    if ( !res.ok ) {
-        throw new Error(`Could not fetch ${url}, received ${res.status}`);
+class SwapiServise {
+    
+    _apiBase = 'https://swapi.dev/api';
+    
+    async getResorse(url) {
+        const res = await fetch(`${this._apiBase}${url}`);
+    
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, received ${res.status}`);
+        };
+    
+        return await res.json();
     };
+    
+    async getAllPeople() {
+        const res = await this.getResorse(`/people/`);
+        return res.results;
+    }
 
-    return await res.json();
+    getPerson(id) {
+        return this.getResorse(`/people/${id}`);
+    }
+
+    async getAllPlenets() {
+        const res = await this.getResorse(`/plenets/`);
+        return res.results;
+    }
+
+    getPlenet(id) {
+        return this.getResorse(`/plenets/${id}`);
+    }
+
+    async getAllStarship() {
+        const res = await this.getResorse(`/starships/`);
+        return res.results;
+    }
+
+    getStarship(id) {
+        return this.getResorse(`/starships/${id}`);
+    }
 };
 
-getResorse('https://swapi.dev/api/people/1/')
-    .then((body) => {
-        console.log(body);
-    }).catch((err) => {
-        console.error("Could  not fetch", err);
-    });
+const swapi = new SwapiServise();
+
+swapi.getAllPeople().then((people) => {
+   people.forEach(element => {
+       console.log(element.name);
+   });
+});
+
+swapi.getPerson(3).then((el) => {
+    console.log(el.name);
+ });
