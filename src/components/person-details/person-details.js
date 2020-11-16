ad0e8539dff2199764795c6b2b661fa69d9c7c61
Roman Pasternak <38011782+RomanPasternak@users.noreply.github.com>
2020-11-16 23:03:01 +0200

    person details with api

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -1,32 +1,68 @@
 import React from 'react';
+import SwapiServise from '../../services/swapi-serwice'
 
 import './person-details.css';
 
-const PersonDetails = () => {
-    return(
-        <div className="person-details card">
-            <img className="person-image"
-                 src="https://starwars-visualguide.com/assets/img/characters/3.jpg" />
+export default class PersonDetails extends React.Component{
+    
+    swapiService = new SwapiServise();
 
-            <div className="card-body">
-                <h4>R2-D2</h4>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item">
-                        <span className="term">Gender</span>
-                        <span>male</span>
-                    </li>
-                    <li className="list-group-item">
-                        <span className="term">Birth Year</span>
-                        <span>43</span>
-                    </li>
-                    <li className="list-group-item">
-                        <span className="term">Eye Color</span>
-                        <span>red</span>
-                    </li>
-                </ul>
+    state = {
+        person: null
+    }
+
+    componentDidMount() {
+        this.updatePerson();
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.personId !== prevProps.personId) {
+            this.updatePerson();
+        }
+    }
+
+    updatePerson() {
+        const { personId } = this.props;
+        if ( !personId ) {
+            return;
+        }
+
+        this.swapiService.getPerson(personId)
+            .then((person) => {
+                this.setState({ person });
+            });
+    };
+    
+    render() {
+
+        if (!this.state.person) {
+            return <span>Select a person from a list</span>
+        };
+
+        const { id, name, gender, birthYear, eyeColor } = this.state.person
+
+        return(
+            <div className="person-details card">
+                <img className="person-image"
+                     src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}  alt="person img"/>
+                <div className="card-body">
+                    <h4>{ name }</h4>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">
+                            <span className="term">Gender</span>
+                            <span>{ gender }</span>
+                        </li>
+                        <li className="list-group-item">
+                            <span className="term">Birth Year</span>
+                            <span>{ birthYear }</span>
+                        </li>
+                        <li className="list-group-item">
+                            <span className="term">Eye Color</span>
+                            <span>{ eyeColor }</span>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    );
+        );
+    };
 };
-
-export default PersonDetails;
